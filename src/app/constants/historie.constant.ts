0b9165ae8ae861d777e1aaf26c504a5036bb9053
pm-ihes <pascal.misler@ihes.io>
2024-03-10 20:55:07 +0100

    Finishing history

--- src/app/constants/historie.constant.ts
+++ src/app/constants/historie.constant.ts
@@ -1,8 +1,34 @@
-export const HistoryItems: { year: string; title: string; texts: string[] }[] = [
+export const HistoryItems: { year: string; title: string; texts: string[]; imgSrc?: string }[] = [
+    {
+        year: '1804',
+        title: 'Firmengründung (22.04.1804)',
+        texts: ['→ Gründung der Firma als Klempner- und Kupferschmiede durch Herrn Paul Lange im Hinterhof der Brückenstraße 10 in Hainichen'],
+        imgSrc: 'assets/images/hist1.png'
+    },
+    {
+        year: '1804',
+        title: 'Verkauf der Firma',
+        texts: [
+            '→ Paul Lange verkauft die Firma an Familie Preuße',
+            '→ Erweiterung des Firmengeländes durch den Anbau eines Ladengeschäfts']
+    },
+    {
+        year: '1838',
+        title: 'Namensänderung der Firma',
+        texts: [
+            '→ Schwiegersohn Gotthelm Preuße übernimmt die Firma',
+            '→ Namensänderung von "Lange" in "Preuße"']
+    },
+    {
+        year: '1875',
+        title: 'Übernahme der Firma',
+        texts: ['→ Gotthelm Preuße übergibt die Firma an seinen Sohn Hermann Preuße']
+    },
     {
         year: '1912',
         title: 'Verkauf der Firma',
-        texts: ['→ Hermann Preuße verkauft die Firma an seinen Schwiegersohn Hermann Enge']
+        texts: ['→ Hermann Preuße verkauft die Firma an seinen Schwiegersohn Hermann Enge'],
+        imgSrc: 'assets/images/hist2.png'
     },
     {
         year: '1920 - 1932',
@@ -18,7 +44,8 @@ export const HistoryItems: { year: string; title: string; texts: string[] }[] = 
         title: 'Absolvierung Meisterprüfung',
         texts: [
             '→ Walter Enge, Sohn von Hermann Enge, absolviert seine Meisterprüfung für das Handwerk der Klempner und Installateure (Gas- und Wasseranlagen) vor der Prüfungskommission und steigt in das Familienunternehmen ein'
-        ]
+        ],
+        imgSrc: 'assets/images/hist3.png'
     },
     {
         year: '1943',
@@ -26,23 +53,19 @@ export const HistoryItems: { year: string; title: string; texts: string[] }[] = 
         texts: [
             '→ Hermann Enge verkauft die Firma an seinen Sohn Walter Enge',
             '→ Spezialisierung auf die Geschäftsbereiche Projektierung und Installation von Heizanlagen, Sanitärinstallation für Firmen, Fabriken und Privatkunden der Stadt Hainichen'
-        ]
+        ],
+        imgSrc: 'assets/images/hist4.png'
     },
     {
         year: '1957 - 1967',
         title: 'Erweiterung des Geschäftsbereichs',
-        texts: [
-            '→ 1957: Kauf des Grundstücks Neumarkt 7 in Hainichen',
-            '→ Erweiterung und Modernisierung des Geschäftsgeländes um zu expandieren'
-        ]
+        texts: ['→ 1957: Kauf des Grundstücks Neumarkt 7 in Hainichen', '→ Erweiterung und Modernisierung des Geschäftsgeländes um zu expandieren'],
+        imgSrc: 'assets/images/hist5.png'
     },
     {
         year: '1967 - 1973',
         title: 'Verkauf des Unternehmens',
-        texts: [
-            '→ 1967: Walter Enge verkauft die Firma an seinen Schwiegersohn Bernd Roscher',
-            '→ 1973: Bernd Roscher wird zum Obermeister gewählt'
-        ]
+        texts: ['→ 1967: Walter Enge verkauft die Firma an seinen Schwiegersohn Bernd Roscher', '→ 1973: Bernd Roscher wird zum Obermeister gewählt']
     },
     {
         year: '1974',
@@ -50,14 +73,13 @@ export const HistoryItems: { year: string; title: string; texts: string[] }[] = 
         texts: [
             '→ Kauf des Grundstücks Neumarkt 7 in Hainichen, welches zu Büroräumen, einer modernen Werkstatt und einem Großlager umgebaut wird',
             '→ Kauf der ersten Firmenfahrzeuge'
-        ]
+        ],
+        imgSrc: 'assets/images/hist6.png'
     },
     {
         year: '1982',
         title: 'Namensänderung der Firma',
-        texts: [
-            '→ Die Firma “Walter Enge” wird in “Bernd Roscher” umbenannt'
-        ]
+        texts: ['→ Die Firma “Walter Enge” wird in “Bernd Roscher” umbenannt']
     },
     {
         year: '1984 - 1991',
@@ -76,7 +98,8 @@ export const HistoryItems: { year: string; title: string; texts: string[] }[] = 
             '→ Einrichtung eines Ladengeschäfts und einer Badausstellung',
             '→ Umwandlung der Firma in die Rechtsform GmbH (01.02.1992)',
             '→ 1996: Uwe Roscher wird zum Geschäftsführer ernannt'
-        ]
+        ],
+        imgSrc: 'assets/images/hist7.png'
     },
     {
         year: '1992 - 2005',
@@ -85,28 +108,25 @@ export const HistoryItems: { year: string; title: string; texts: string[] }[] = 
             '→ 1992: Expansion in den Bereich der Gasbrennwerttechnik',
             '→ 2001: Expansion in den Geschäftsbereich des thermischen Solars',
             '→ 2005: Erweiterung des Geschäftsbereiches auf die Installation von Blockheizkraftwerken (BHKW und KWK)'
-
-        ]
+        ],
+        imgSrc: 'assets/images/hist8.png'
     },
     {
         year: '2005',
         title: 'Übernahme des Unternehmens',
-        texts: [
-            '→ Uwe Roscher übernimmt die Geschäftsführung des Unternehmens von seinem Vater Bernd Roscher'
-        ]
+        texts: ['→ Uwe Roscher übernimmt die Geschäftsführung des Unternehmens von seinem Vater Bernd Roscher']
     },
     {
         year: '2010',
         title: 'Erweiterung der Kompetenzbereiche',
-        texts: [
-            '→ Expansion in den Geschäftsbereich der Wärmepumpen, welches den Grundstein der heutigen Erneuerbaren Energien bildet'
-        ]
+        texts: ['→ Expansion in den Geschäftsbereich der Wärmepumpen, welches den Grundstein der heutigen Erneuerbaren Energien bildet']
     },
     {
         year: '2022',
         title: '30-jähriges Jubiläum (GmbH)',
         texts: [
             '→ Oberbürgermeister Dieter Greysinger (links) gratuliert und zeichnet die Roscher GmbH für ihr 30-jähriges Bestehen als GmbH aus mit anschließender Jubiläumsfeier'
-        ]
-    },
+        ],
+        imgSrc: 'assets/images/hist9.png'
+    }
 ];
