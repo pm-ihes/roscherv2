06935e1f7dc74878edab59b351a72b85cd2e9d88
pm-ihes <pascal.misler@ihes.io>
2024-04-22 14:23:38 +0200

    WIP

--- src/app/constants/meta.constant.ts
+++ src/app/constants/meta.constant.ts
@@ -1,2 +1,2 @@
-export const CONTACT_URL: string = 'https://pmdevpm.de/contact/contact';
-export const APPLICATION_URL: string = 'https://pmdevpm.de/contact/application';
+export const CONTACT_URL: string = 'https://pmdevpm.de/contact/contact.php';
+export const APPLICATION_URL: string = 'https://pmdevpm.de/contact/application.php';
